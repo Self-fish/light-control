5ca96a38c57820e42f1fb816c75d5c25858c4006
pablo <pablo@pablos-MacBook-Pro.local>
2022-01-16 21:16:22 +0100

    Minor change

--- domain/model/LightPreferences.py
+++ domain/model/LightPreferences.py
@@ -6,8 +6,11 @@ from domain.model.LightPreferencesSource import LightPreferencesSource
 class LightPreferences:
 
     def __init__(self, starting_hour, finishing_hour, light_mode: LightMode, source: LightPreferencesSource):
-        self.__starting_hour = starting_hour
-        self.__finishing_hour = finishing_hour
-        self.__light_mode = light_mode
-        self.__source = source
+        self.starting_hour = starting_hour
+        self.finishing_hour = finishing_hour
+        self.light_mode = light_mode
+        self.source = source
+
+
+
 
