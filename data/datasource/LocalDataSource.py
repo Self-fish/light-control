f205b4ae1b280305a949b1a4fc4f25ab0025c4a8
pablo <pablo@pablos-MacBook-Pro.local>
2022-02-02 23:03:50 +0100

    Using as local data source, a file

--- data/datasource/LocalDataSource.py
+++ data/datasource/LocalDataSource.py
@@ -3,4 +3,7 @@ from domain.model.LightMode import LightMode
 
 
 def get_light_preferences():
+    with open('lighs.conf') as file:
+        file_contents = file.read()
+        print(file_contents)
     return LightPreferencesDataModel("14:00", "23:59", LightMode.AUTOMATIC)
