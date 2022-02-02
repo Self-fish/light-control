5b60294f5a106870ec27bf2074d7e79e974fecb7
pablo <pablo@pablos-MacBook-Pro.local>
2022-02-02 23:43:03 +0100

    minor changes

--- data/datasource/LocalDataSource.py
+++ data/datasource/LocalDataSource.py
@@ -23,4 +23,4 @@ def get_light_preferences():
     print(starting_hour)
     print(finishing_hour)
     print(mode)
-    return LightPreferencesDataModel(starting_hour, finishing_hour, LightMode(mode[:-1]))
+    return LightPreferencesDataModel(starting_hour, finishing_hour, LightMode("MANUAL_ON"))
