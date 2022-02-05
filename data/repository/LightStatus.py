3a4143a4e79acaf8e90b930921e604e480b77bcb
pablo <pablo@pablos-MacBook-Pro.local>
2022-02-05 11:38:06 +0100

    Turning off lights by default when starting

--- data/repository/LightStatus.py
+++ data/repository/LightStatus.py
@@ -1,4 +1,5 @@
 from data.driver.RelayController import RelayController
+from data.driver.RelayStatus import RelayStatus
 
 
 class LightStatusRepository:
@@ -6,6 +7,7 @@ class LightStatusRepository:
     def __init__(self, light_controller: RelayController):
         self.__light_controller = light_controller
         self.current_light_status = self.__light_controller.get_current_relay_status()
+        self.__light_controller.update_relay_status(RelayStatus.OFF)
 
     def update_light_status(self, light_status):
         if light_status != self.current_light_status:
